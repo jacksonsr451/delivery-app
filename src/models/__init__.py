dfd624c993179cfad3180ee141a5820306f7dcf1
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-12-04 22:37:07 -0300

    create users model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -7,3 +7,4 @@ from .role_model import RoleModel
 from .address_model import AddressModel
 from .contact_model import ContactModel
 from .profile_model import ProfileModel
+from .user_model import UserModel
---
+++ src/models/user_model.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from src.core.database import Base
+
+
+class UserModel(Base):
+    __tablename__ = 'users'
+
+    id = Column(String, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+    role_id = Column(String, ForeignKey('roles.id'))
+    role = relationship('RoleModel', foreign_keys=[role_id])
+    profile_id = Column(String, ForeignKey('profiles.id'))
+    profile = relationship('ProfileModel', foreign_keys=[profile_id])
+    contact_id = Column(String, ForeignKey('contacts.id'))
+    contact = relationship('ContactModel', foreign_keys=[contact_id])
+    address_id = Column(String, ForeignKey('addresses.id'))
+    address = relationship('AddressModel', foreign_keys=[address_id])
