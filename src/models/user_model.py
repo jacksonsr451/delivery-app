5cea90214a32c4613d8c7b51456764b33aed5642
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-12-05 19:56:34 -0300

    refactoring auth request

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,11 +1,6 @@
 from sqlalchemy import Column, String, Boolean
-from sqlalchemy.orm import relationship
 
 from src.core.database import Base
-from src.models.address_model import AddressModel
-from src.models.contact_model import ContactModel
-from src.models.profile_model import ProfileModel
-from src.models.role_model import RoleModel
 
 
 class UserModel(Base):
@@ -14,10 +9,6 @@ class UserModel(Base):
     id = Column(String, primary_key=True)
     username = Column(String)
     password = Column(String)
-    role = relationship('RoleModel', foreign_keys=[RoleModel.id])
-    profile = relationship('ProfileModel', foreign_keys=[ProfileModel.id])
-    contact = relationship('ContactModel', foreign_keys=[ContactModel.id])
-    address = relationship('AddressModel', foreign_keys=[AddressModel.id])
     status = Column(Boolean)
 
     def __init__(self, data: object) -> None:
--- src/requests/auth_request.py
+++ src/requests/auth_request.py
@@ -11,9 +11,9 @@ class AuthRequest:
     status: bool
 
     def __init__(self, schema: Request) -> None:
-        self.id = uuid4()
+        self.id = uuid4().__str__()
         self.username = schema.form['username']
         self.password = hashpw(
             schema.form['password'].encode('utf8'), gensalt()
-        )
+        ).decode('utf8')
         self.status = False
