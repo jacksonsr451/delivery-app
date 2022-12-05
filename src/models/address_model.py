9ccd990566f521e8fafbdba6d41b55ec3755e927
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-12-05 08:32:16 -0300

    add constructor in AddressModel

--- src/models/address_model.py
+++ src/models/address_model.py
@@ -1,6 +1,8 @@
+from uuid import uuid4
 from sqlalchemy import Column, String
 
 from src.core.database import Base
+from src.requests.address_request import AddressRequest
 
 
 class AddressModel(Base):
@@ -12,3 +14,11 @@ class AddressModel(Base):
     neigborhod = Column(String)
     street = Column(String)
     number = Column(String)
+
+    def __init__(self, request: AddressRequest) -> None:
+        self.id = request.id if request.id else uuid4()
+        self.country = request.country
+        self.state = request.state
+        self.neigborhod = request.neigborhod
+        self.street = request.street
+        self.number = request.number
