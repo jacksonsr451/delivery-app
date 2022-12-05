73aa91470b7075cced92b9c8c5035d10661c8b3c
jacksonsr45 <jackson.severino.rocha@escola.pr.gov.br>
2022-12-04 22:12:13 -0300

    Create model offers

---
+++ src/models/offer_model.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, Date, ForeignKey, Float
+from sqlalchemy.orm import relationship
+
+from src.core.database import Base
+
+
+class OfferModel(Base):
+    __tablename__ = 'offers'
+
+    id = Column(String, primary_key=True)
+    slug = Column(String)
+    category_id = Column(String, ForeignKey('categories.id'))
+    category = relationship('CategoryModel', foreign_keys=[category_id])
+    product_id = Column(String, ForeignKey('products.id'))
+    product = relationship('ProductModel', foreign_keys=[product_id])
+    discount = Column(Float)
+    created_at = Column(Date)
+    valid_until = Column(Date)
